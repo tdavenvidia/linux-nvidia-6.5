6d44b02c86d55c560975856b883a80ca2613b896
Jason Gunthorpe <jgg@nvidia.com>
2023-09-27 20:47:34 -0300

    iommufd: Convert to alloc_domain_paging()
    Move the global static blocked domain to the ops and convert the unmanaged
    domain to domain_alloc_paging.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Acked-by: Sven Peter <sven@svenpeter.dev>
    Link: https://lore.kernel.org/r/4-v2-bff223cf6409+282-dart_paging_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommufd/selftest.c
+++ drivers/iommu/iommufd/selftest.c
@@ -455,11 +455,13 @@ static const struct iommu_ops mock_ops = {
 	 * because it is zero.
 	 */
 	.default_domain = &mock_blocking_domain,
+	.blocked_domain = &mock_blocking_domain,
 	.owner = THIS_MODULE,
 	.pgsize_bitmap = MOCK_IO_PAGE_SIZE,
 	.hw_info = mock_domain_hw_info,
 	.domain_alloc = mock_domain_alloc,
 	.domain_alloc_user = mock_domain_alloc_user,
+	.domain_alloc_paging = mock_domain_alloc_paging,
 	.capable = mock_domain_capable,
 	.device_group = generic_device_group,
 	.probe_device = mock_probe_device,
